94005a71358bf58fd3b64e8453fc1fe76e4e29f3
Edward Pie <hackstockpie@gmail.com>
2024-03-26 21:31:47 +0100

    resolves media root

--- orijinstores/orijinstores/urls.py
+++ orijinstores/orijinstores/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("stories/", include("stories.urls")),
     path("admin/", admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
